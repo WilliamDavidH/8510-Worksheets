f4110547dbfdc80b36dd7e5c8870121392d1afbb
William Hiott <hiottw@node0049.palmetto.clemson.edu>
2024-02-01 13:16:03 -0500

    add class notes

---
+++ Class notes 1-31-2024.R
@@ -0,0 +1,83 @@
+bengals <- 2
+bills <- 2
+
+if (bengals > bills) {
+  print("Bengals win")
+}  else if (bills > bengals) {
+  print("Bills win")
+}  else { 
+  print("There was a tie")
+}
+
+presidents <- c("Wahington", "Jefferson, "Adams")
+
+for (m in presidents) {
+  print(m)
+}
+
+for (m in length(presidents)) 
+print (m)
+}
+# 
+
+pagenumber <-1
+while (pagenumber <= 10) {
+  print(paste("the page number is", pagenumber, ""))
+  pagenumber <- pagenumber + 1
+  print(paste("the page number is at ", pagenumber))
+}
+
+for (p in length BostonWomensVoters$occupation)) {
+if (p == "Housewife" or "Stenographer")
+print(BostonWomansVoters$Name) {
+else")
+
+{1 BostonWomenVoters}
+
+print(occupation "BostonWomensVoters")
+
+
+pagenumber <- pagenumber + 1 
+
+test_ex[re{}])
+{
+if (occupation ("housewife","stenographyer') {
+  print("name")
+}  
+else if () {
+  print("Bills win")
+}  else { 
+  print("There was a tie")
+}
+
+for (p in |:(BostonWomensVoters)) {
+
+for (p in 1:length ((BostonWomensVoters$Occupation)) {
+if (p == "housewife" or "stenographer""} 
+print(BostonWomenVoters$name)
+}
+}
+
+
+for (i in length(president)) {
+  print presidents
+  
+  
+for (x in 1:length(BostonWomenVoters$Occupation))  {
+  #print(BostonWomenVoters$Occupation[x])
+
+  if(BostonWomenVoters$Occupation[x] == "Housewife" | "Stenographer") {
+    print("this woman is a housewife")
+  }
+  
+y < -1:length(BostonWomenVoters$Occupation)
+print(paste("and her name is ", BostonWomenVoters$Name))
+} else if(BostonWomenVoters$Occupation[x] == Stenographer)
+  print("this woman is a stenographer")
+  print (paste("and her name is", BostonWomenVoters))
+}
+  }
+  
+for (x in )  
+}
+}
